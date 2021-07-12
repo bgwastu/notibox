532b5e878e1f263f70f42ddaf39c9ed44801cfbd
atticdev <67826350+bgwastu@users.noreply.github.com>
2021-07-12 19:36:51 +0700

    Completed Notion provider

--- lib/app/data/auth_provider.dart
+++ lib/app/data/auth_provider.dart
@@ -1,12 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 import 'package:notibox/app/config/constants.dart';
+import 'package:notibox/app/data/model/inbox_model.dart';
+import 'package:notibox/app/data/repository/settings_repository.dart';
 
 class AuthProvider {
   final _dio = Get.put(Dio());
 
   Future<bool> checkToken(String token) async {
-
     try {
       final response = await _dio.get(BASE_URL + 'users',
           options: Options(headers: {
@@ -15,10 +16,40 @@ class AuthProvider {
           }));
       return response.statusCode == 200;
     } on DioError catch (e) {
-      if(e.response!.statusCode == 401){
+      if (e.response!.statusCode == 401) {
         return false;
       }
       throw e;
     }
+  }
+
+  Future<List<Inbox>> getListInbox({required String databaseId}) async {
+    final token = (await SettingsRepository.instance()).getToken();
+    final res = await Dio().post('$BASE_URL/databases/$databaseId/query',
+        options: Options(headers: {
+          'Authorization': 'Bearer ' + token,
+          'Notion-Version': NOTION_VERSION
+        }),
+        data: {
+          'sorts': [
+            {'timestamp': 'created_time', 'direction': 'descending'}
+          ]
+        });
+    return (res.data['results'] as List).map((e) => Inbox.fromMap(e)).toList();
+  }
+
+  Future createInbox({required Inbox inbox, required String databaseId}) async {
+    final token = (await SettingsRepository.instance()).getToken();
+    await Dio().post('$BASE_URL/pages',
+        data: {
+          'parent': {
+            'database_id': databaseId,
+          },
+          'properties': inbox.toMap()
+        },
+        options: Options(headers: {
+          'Authorization': 'Bearer ' + token,
+          'Notion-Version': NOTION_VERSION
+        }));
   }
 }
---
+++ lib/app/data/model/inbox_model.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Inbox {
+  final String? pageId;
+  final String title;
+  final String? description;
+  final DateTime reminder;
+  final Select labels;
+
+  Inbox({
+    this.pageId,
+    required this.title,
+    required this.description,
+    required this.reminder,
+    required this.labels,
+  });
+
+  Map<String, dynamic> toMap(){
+  final dateFormat = DateFormat('yyyy-dd-MM');
+  return {
+    'Title': {'title': [{'text': {'content': title}}]},
+    'Description': {'rich_text': [{'text': {'content': description}}]},
+    'Reminder': {'date': {'start': dateFormat.format(reminder)}},
+    'Label': {'select': labels.toMap()}
+  };
+  }
+
+  factory Inbox.fromMap(Map<String, dynamic> map) {
+    final properties = map['properties'] as Map<String, dynamic>;
+    final listTitle = (properties['Title']['title'] ?? []) as List;
+    final description = properties['Description']?['rich_text'] as List;
+    final labels = Select.fromMap(properties['Label']?['select']);
+
+    return Inbox(
+      pageId: map['id'],
+      title: listTitle.isNotEmpty ? listTitle[0]['plain_text'] : 'Untitled',
+      reminder: DateTime.tryParse(properties['Reminder']?['date']?['start']) ??
+          DateTime(0),
+      labels: labels,
+      description: description.isNotEmpty ? description[0]['plain_text'] : '-',
+    );
+  }
+}
+
+class Select {
+  final String? id;
+  final String name;
+  final Color? color;
+
+  Select({this.id, required this.name, this.color});
+  factory Select.fromMap(Map<String, dynamic> map) {
+    late Color color;
+    switch (map['color']) {
+      case 'default':
+        color = Color(0xFFE6E6E4);
+        break;
+      case 'gray':
+        color = Color(0xFFD7D7D5);
+        break;
+      case 'brown':
+        color = Color(0xFFE8D5CC);
+        break;
+      case 'orange':
+        color = Color(0xFFFDDFCC);
+        break;
+      case 'yellow':
+        color = Color(0xFFFBEECC);
+        break;
+      case 'green':
+        color = Color(0xFFCCE7E1);
+        break;
+      case 'blue':
+        color = Color(0xFFCCE4F9);
+        break;
+      case 'purple':
+        color = Color(0xFFE1D3F8);
+        break;
+      case 'pink':
+        color = Color(0xFFF8CCE6);
+        break;
+      case 'red':
+        color = Color(0xFFFFCCD1);
+        break;
+      default:
+        color = Color(0xFFE6E6E4);
+        break;
+    }
+    return Select(id: map['id'], name: map['name'], color: color);
+  }
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+    };
+  }
+}
