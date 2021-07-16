bb066a5cf22c484f2902f925d7e4ae0f70396386
atticdev <67826350+bgwastu@users.noreply.github.com>
2021-07-16 08:07:36 +0700

    Add Firebase messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
@@ -12,9 +13,8 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await hiveInit();
-  await Firebase.initializeApp();
   notificationInit();
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  await _firebaseInit();
   runApp(
     GetMaterialApp(
       title: "Notibox",
@@ -23,12 +23,26 @@ void main() async {
       theme: AppThemes.lightTheme,
       builder: EasyLoading.init(),
       darkTheme: AppThemes.darkTheme,
-      themeMode: SettingsRepository.isDarkMode() == null ? ThemeMode.system : SettingsRepository.isDarkMode()! ? ThemeMode.dark : ThemeMode.light,
+      themeMode: SettingsRepository.isDarkMode() == null
+          ? ThemeMode.system
+          : SettingsRepository.isDarkMode()!
+              ? ThemeMode.dark
+              : ThemeMode.light,
       debugShowCheckedModeBanner: false,
       enableLog: true,
     ),
   );
   easyLoadingConfig();
+}
+
+Future<void> _firebaseInit() async {
+  await Firebase.initializeApp();
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  AwesomeNotifications().createNotificationFromJsonData(message.data);
 }
 
 void notificationInit() {
